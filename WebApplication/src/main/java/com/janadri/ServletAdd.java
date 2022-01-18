47fe2ad9cb370b151a88f0ecff3932e38d050b2a
Veeru <Veeru@Itachi-Uchiha>
2022-01-18 21:33:53 +0530

    Updating the project

--- WebApplication/src/main/java/com/janadri/ServletAdd.java
+++ WebApplication/src/main/java/com/janadri/ServletAdd.java
@@ -7,15 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ServletAdd extends HttpServlet{
+public class ServletAdd extends HttpServlet {
 
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
-		
+
 		int k = i + j;
 		PrintWriter out = res.getWriter();
-		
-		out.println("The sum is : "+k);
+
+		out.println("The sum is : " + k);
 	}
 }
