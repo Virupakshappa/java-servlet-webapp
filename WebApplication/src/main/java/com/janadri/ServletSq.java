ebd2900dcc6ae6840646c30eedd72cc42d7d5d4b
Veeru <vjanadri7@gmail.com>
2022-01-19 19:26:22 +0530

    Updated the application with httpSession

--- WebApplication/src/main/java/com/janadri/ServletSq.java
+++ WebApplication/src/main/java/com/janadri/ServletSq.java
@@ -6,13 +6,17 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ServletSq extends HttpServlet{
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
+		HttpSession session = req.getSession();
 		
-		int k = Integer.parseInt(req.getParameter("k")) ;
+//		session.removeAttribute("k");        //removes the attribute from the session
+		
+		int k = (int) session.getAttribute("k");
 		
 		k = k*k;
 		
